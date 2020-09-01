7a56b78821b144c686f9efb55393e7e073941523
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-09-01 14:36:17 +0900

    First Release

---
+++ src/components/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { MovesInput, PlayButton, PauseButton, ForwardButton, ReverseButton,
+  AddMinutesButton, NavigationButton, ElapsedTimeValue, ElapsedTimeRange,
+  SpeedValue, SpeedRange } from 'harmoware-vis';
+import { ObjectInput } from './object-input.js';
+
+export default class Controller extends React.Component {
+  updatePos(idx,e){
+    const { position, updateState } = this.props;
+    position[idx] = Number(e.target.value);
+    updateState({position})
+  }
+  updateOri(idx,e){
+    const { getOrientation, updateState } = this.props;
+    getOrientation[idx] = Number(e.target.value);
+    updateState({getOrientation})
+  }
+
+  render() {
+    const { actions, inputFileName, animatePause, animateReverse, viewport, leading,
+      settime, timeBegin, timeLength, secperhour, objFileName, position, getOrientation, updateState } = this.props;
+    const { movesFileName } = inputFileName;
+
+    return (
+        <div className="harmovis_controller">
+            <ul className="flex_list">
+            <li className="flex_row">
+                <div className="harmovis_input_button_column">
+                <label htmlFor="ObjectInput">
+                3D object data selection<ObjectInput actions={actions} id="ObjectInput" updateState={updateState}/>
+                </label>
+                <div>{objFileName}</div>
+                </div>
+            </li>
+            <li className="flex_column">
+              longitude:<input type="number" value={position[0]} step="0.0001" onChange={this.updatePos.bind(this,0)} required/>
+              latitude:<input type="number" value={position[1]} step="0.0001" onChange={this.updatePos.bind(this,1)} required/>
+              altitude:<input type="number" value={position[2]} step="0.1" onChange={this.updatePos.bind(this,2)} required/>
+              pitch:<input type="number" value={getOrientation[0]} step="1" onChange={this.updateOri.bind(this,0)} required/>
+              yaw:<input type="number" value={getOrientation[1]} step="1" onChange={this.updateOri.bind(this,1)} required/>
+              roll:<input type="number" value={getOrientation[2]} step="1" onChange={this.updateOri.bind(this,2)} required/>
+            </li>
+            <li className="flex_row">
+                <div className="harmovis_input_button_column">
+                <label htmlFor="MovesInput">
+                PointCloud data selection<MovesInput actions={actions} id="MovesInput"/>
+                </label>
+                <div>{movesFileName}</div>
+                </div>
+            </li>
+            <li className="flex_row">
+              {animatePause ?
+                <PlayButton actions={actions} />:<PauseButton actions={actions} />
+              }&nbsp;
+              {animateReverse ?
+                <ForwardButton actions={actions} />:<ReverseButton actions={actions} />
+              }
+            </li>
+            <li className="flex_row">
+              <AddMinutesButton addMinutes={-10} actions={actions} />&nbsp;
+              <AddMinutesButton addMinutes={-5} actions={actions} />
+            </li>
+            <li className="flex_row">
+              <AddMinutesButton addMinutes={5} actions={actions} />&nbsp;
+              <AddMinutesButton addMinutes={10} actions={actions} />
+            </li>
+            <li className="flex_row">
+              <NavigationButton buttonType="zoom-in" actions={actions} viewport={viewport} />&nbsp;
+              <NavigationButton buttonType="zoom-out" actions={actions} viewport={viewport} />&nbsp;
+              <NavigationButton buttonType="compass" actions={actions} viewport={viewport} />
+            </li>
+            <li className="flex_column">
+              <label htmlFor="ElapsedTimeRange">elapsedTime
+              <ElapsedTimeValue settime={settime} timeBegin={timeBegin} timeLength={timeLength} actions={actions}
+              min={leading*-1} />
+              sec</label>
+              <ElapsedTimeRange settime={settime} timeLength={timeLength} timeBegin={timeBegin} actions={actions}
+              min={leading*-1} id="ElapsedTimeRange" />
+            </li>
+            <li className="flex_column">
+              <label htmlFor="SpeedRange">speed
+              <SpeedValue secperhour={secperhour} actions={actions} />sec/hour</label>
+              <SpeedRange secperhour={secperhour} actions={actions} id="SpeedRange" />
+            </li>
+            </ul>
+        </div>
+    );
+  }
+}
---
+++ src/containers/app.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { PointCloudLayer, SimpleMeshLayer } from 'deck.gl';
+import {
+  Container, connectToHarmowareVis, HarmoVisLayers, LoadingIcon, FpsDisplay
+} from 'harmoware-vis';
+import Controller from '../components';
+
+const MAPBOX_TOKEN = process.env.MAPBOX_ACCESS_TOKEN; //Acquire Mapbox accesstoken
+
+class App extends Container {
+  constructor(props) {
+    super(props);
+    this.state = {
+      popup: [0, 0, ''],
+      objFileName: '',
+      objFileData: null,
+      position:[136.906428,35.181453,0],
+      getColor:[255,255,255,255],
+      getOrientation:[0,0,90],
+      opacity: 0.1,
+    };
+  }
+
+  componentDidMount(){
+    super.componentDidMount();
+    const { actions } = this.props;
+    actions.setSecPerHour(3600);
+    actions.setLeading(3);
+    actions.setTrailing(3);
+    actions.setViewport({longitude:136.906428,latitude:35.181453,zoom:18.5,maxZoom:25,pitch:60,maxPitch:90});
+    actions.setDefaultViewport({defaultZoom:18.6,defaultPitch:60});
+  }
+
+  updateState(updateData){
+    this.setState(updateData);
+  }
+
+  onHover(el) {
+    if (el && el.object) {
+      let disptext = '';
+      const objctlist = Object.entries(el.object);
+      for (let i = 0, lengthi = objctlist.length; i < lengthi; i=(i+1)|0) {
+        const strvalue = objctlist[i][1].toString();
+        disptext = disptext + (disptext.length > 0 ? '\n' : '');
+        disptext = disptext + (`${objctlist[i][0]}: ${strvalue}`);
+      }
+      this.setState({ popup: [el.x, el.y, disptext] });
+    } else {
+      this.setState({ popup: [0, 0, ''] });
+    }
+  }
+
+  getPointCloudLayer(PointCloudData){
+    return PointCloudData.map((pointCloudElements, idx)=>{
+      const {pointCloud} = pointCloudElements;
+      const data = pointCloud.filter(x=>x.position);
+      const onHover = this.onHover.bind(this);
+      return new PointCloudLayer({
+        id: 'PointCloudLayer-' + String(idx),
+        data,
+        getColor: x => x.color || [0,255,0,128+x.position[3]*1.28],
+        sizeUnits: 'meters',
+        pointSize: 0.1,
+        onHover
+      });
+    });
+  }
+
+  render() {
+    const { actions, viewport, movedData, loading } = this.props;
+    const PointCloudData = movedData.filter(x=>x.pointCloud);
+
+    return (
+      <div>
+        <Controller {...this.props} {...this.state} updateState={this.updateState.bind(this)} />
+        <div className="harmovis_area">
+          <HarmoVisLayers
+            viewport={viewport} actions={actions} visible={false}
+            layers={[
+                new SimpleMeshLayer({
+                  id:'SimpleMeshLayer',
+                  data:[{position:this.state.position}],
+                  mesh:this.state.objFileData,
+                  getColor:this.state.getColor,
+                  getOrientation:this.state.getOrientation,
+                  opacity:this.state.opacity,
+                }),
+                PointCloudData.length > 0 ? this.getPointCloudLayer(PointCloudData):null,
+            ]}
+          />
+        </div>
+        <div className="harmovis_footer">
+          longitude:{viewport.longitude}&nbsp;
+          latitude:{viewport.latitude}&nbsp;
+          zoom:{viewport.zoom}&nbsp;
+          bearing:{viewport.bearing}&nbsp;
+          pitch:{viewport.pitch}
+        </div>
+        <svg width={viewport.width} height={viewport.height} className="harmovis_overlay">
+          <g fill="white" fontSize="12">
+            {this.state.popup[2].length > 0 ?
+              this.state.popup[2].split('\n').map((value, index) =>
+                <text
+                  x={this.state.popup[0] + 10} y={this.state.popup[1] + (index * 12)}
+                  key={index.toString()}
+                >{value}</text>) : null
+            }
+          </g>
+        </svg>
+        <LoadingIcon loading={loading} />
+        <FpsDisplay />
+      </div>
+    );
+  }
+}
+export default connectToHarmowareVis(App);
